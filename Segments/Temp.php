158622b8fda966a765bb93c5e7386632d4b2da35
jivko <jivko.jechev@investor.bg>
2017-12-14 11:14:38 +0200

    fix temp

--- Segments/Temp.php
+++ Segments/Temp.php
@@ -5,9 +5,13 @@ class Temp extends AbstractSegment implements SegmentInterface
 
     public function getSegmentData()
     {
+        $out = "";
         $temp = (int) @trim(file_get_contents("/sys/class/thermal/thermal_zone0/temp"));
         $temp = (float) $temp / 1000;
-        return $temp."°c";
+        if ($temp) {
+             $out = $temp . "°c";
+        }
+        return $out;
     }
 
 }
